f982f019878916de49d243e451335090f18e8558
kyb024 <kim.yongbeom@hotmail.com>
2022-06-21 14:36:05 +0900

    클래스 연습4

--- isinstace함수_파생여부확인.py
+++ isinstace함수_파생여부확인.py
@@ -2,8 +2,12 @@ class Person:
     pass
 class Bird:
     pass
+
+#부모 클래스인 Person을 상속받아서 자식 클래서 Student 정의
 class Student(Person):
     pass
+
+#인스턴스를 생성
 p, s = Person(), Student()
 
 print("p is instance of Person: ", isinstance(p, Person))
@@ -11,3 +15,4 @@ print("s is instance of Person: ", isinstance(s, Person))
 print("p is instance of Object: ", isinstance(p, object))
 print("p is instance of Bird: ", isinstance(p, Bird))
 print("int is instance of Object: ", isinstance(int, object))
+
